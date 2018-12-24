008fea9dc6f9c46fb1da4204c76ff6f13d1e89a0
LW <1778768609@qq.com>
2018-12-24 13:54:29 +0800

    我的报事已完成，未完成完成 # 你能看的懂吗

--- script/static.js
+++ script/static.js
@@ -1,5 +1,45 @@
-function Static() {
-}
+(function (window) {
+    //数据字典
+    var Static = function Static() {
+        return new Static.prototype.init();
+    };
+    Static.prototype = {
+        constructor: Static,
+        DICTION: 'DICTION',
+        init: function () {
+            this.getAllDiction();
+        },
+        //获取所有的数据字典
+        getAllDiction: function () {
+            api.ajax({
+                url: Static.IP + '/api/app/communitywanli/dictionary/listDictionaryByParentCode',
+                method: 'get',
+                dataType: 'json'
+            }, function (ret, err) {
+                if (ret) {
+                    var list = ret.object;
+                    if (list && list.length > 0) {
+                        var storage = window.localStorage;
+                        for (var i = 0; i < list.length; i++) {
+                            storage.setItem(list[i].code, list[i].name);
+                        }
+                    }
+                }
+            });
+        }
+    };
+    Static.getDictionByCode = function (code) {
+        var info = window.localStorage.getItem(code);
+        if (!info) {
+            this.getAllDiction();
+            return window.localStorage.getItem(code);
+        }
+        return info;
+    };
+    Static.prototype.init.prototype = Static.prototype;
+    window.Static = Static;
+})(window);
+
 
 //访问地址
 Static.IP = 'http://192.168.1.222:9765';
